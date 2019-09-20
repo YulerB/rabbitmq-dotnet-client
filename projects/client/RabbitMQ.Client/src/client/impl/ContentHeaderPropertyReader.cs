ea9b3df300c3b170776f4245292a6d3689a07daf
Brian Yule <brian.yule@hotmail.com>
2019-09-20 10:09:03 +0100

    updating how we handle presence for basic properties.

--- projects/client/RabbitMQ.Client/src/client/impl/ContentHeaderPropertyReader.cs
+++ projects/client/RabbitMQ.Client/src/client/impl/ContentHeaderPropertyReader.cs
@@ -46,44 +46,13 @@ namespace RabbitMQ.Client.Impl
 {
     public class ContentHeaderPropertyReader2
     {
-        protected ushort m_bitCount;
-        protected ushort m_flagWord;
         private ArraySegmentSequence BaseReader;
 
         public ContentHeaderPropertyReader2(ArraySegmentSequence reader)
         {
             BaseReader = reader;
-            m_flagWord = 1; // just the continuation bit
-            m_bitCount = 15; // the correct position to force a m_flagWord read
         }
         
-        public bool ContinuationBitSet
-        {
-            get { return ((m_flagWord & 1) != 0); }
-        }
-
-        public void FinishPresence()
-        {
-            if (ContinuationBitSet)
-            {
-                throw new MalformedFrameException("Unexpected continuation flag word");
-            }
-        }
-
-        public bool ReadBit()
-        {
-            return ReadPresence();
-        }
-
-        public void ReadFlagWord()
-        {
-            if (!ContinuationBitSet)
-            {
-                throw new MalformedFrameException("Attempted to read flag word when none advertised");
-            }
-            m_flagWord = BaseReader.ReadUInt16();
-            m_bitCount = 0;
-        }
 
         public uint ReadUInt32()
         {
@@ -103,19 +72,6 @@ namespace RabbitMQ.Client.Impl
         public byte ReadOctet()
         {
             return BaseReader.ReadByte();
-        }
-
-        public bool ReadPresence()
-        {
-            if (m_bitCount == 15)
-            {
-                ReadFlagWord();
-            }
-
-            int bit = 15 - m_bitCount;
-            bool result = (m_flagWord & (1 << bit)) != 0;
-            m_bitCount++;
-            return result;
         }
 
         public ushort ReadShort()
